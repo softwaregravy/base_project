747c410635990164e69f361f05f3cfdd6080fc40
John Hinnegan <john.hinnegan@gmail.com>
2011-02-15 19:50:06 -0500

    delayed jobs working

--- spec/controllers/sanity_controller_spec.rb
+++ spec/controllers/sanity_controller_spec.rb
@@ -20,6 +20,13 @@ describe SanityController do
         post method, {:dummy => "string"} 
       }.should_not change(Dummy, :count)
     end 
+
+    it "the asynchronous job should create a Dummy" do 
+      post method, {:dummy => "string"} 
+      lambda {
+        Delayed::Worker.new.work_off
+      }.should change(Dummy, :count).by 1
+    end 
   end 
 
   describe "#delayed_method_call" do 
@@ -27,26 +34,6 @@ describe SanityController do
   end 
 
   describe "#delayed_job" do 
-
     it_should_behave_like "something creating a delayed job", 'delayed_job'
-
-    it "should create a delayed job" do 
-      lambda { 
-        post 'delayed_job', {:dummy => "string"} 
-      }.should change(Delayed::Job, :count).by 1
-    end 
-
-    it "an asynchronous job should run" do 
-      post 'delayed_job', {:dummy => "string"} 
-      Delayed::Worker.new.work_off.should == [1, 0] # indicates [#success, #failure]
-    end 
-
-    it "the asynchronous job should create a Dummy" do 
-      post 'delayed_job', {:dummy => "string"} 
-      lambda {
-        Delayed::Worker.new.work_off
-      }.should change(Dummy, :count).by 1
-    end 
-
   end 
 end
