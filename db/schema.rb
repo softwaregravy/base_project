12d8f67cebfe6fd0570f9e129cf007175b759021
John Hinnegan <john.hinnegan@gmail.com>
2011-02-21 15:04:26 -0500

    remove dummy stuff

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110215175048) do
+ActiveRecord::Schema.define(:version => 20110215174325) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,1 @@
-Factory.define :dummy do  |dummy|
-  dummy.my_field 'DUMMY' 
-end                                                                                                                                                                                                                                                                         
-
-Factory.define :person, :class => Dummy do  |person|
-  person.my_field 'PERSON' 
-end 
 
